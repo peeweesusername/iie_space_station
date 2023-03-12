05662d7410dc9b22d8130585a08576e5bf251aa9
peeweesusername <92876718+peeweesusername@users.noreply.github.com>
2023-03-12 07:53:51 -0400

    fatter laserbolt

--- lib/globals.dart
+++ lib/globals.dart
@@ -9,7 +9,7 @@ const double directionButtonSize = 12.0;
 const double fireButtonSize = 12.0;
 
 const double laserBoltL = 2.0;
-const double laserBoltW = 0.4;
+const double laserBoltW = 1.0;
 const double lbSpeed  = 40.0;
 
 const double alienShipSize = 5.0;
